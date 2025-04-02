65c1efd5979d1c783f00a7dd0c572ea38afb07ee
Zrantrontran <fabilopezlu@hotmail.com>
2025-04-02 19:58:50 +0200

    B: added clock timer and created base for character shape

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while(1):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -19,6 +21,7 @@ def main():
 
         pygame.Surface.fill(screen, 0)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
